384c3e0c9b2b16d72f3bcbd7010c2dae8115ba9d
Dot Growen <60196575+dotgrowen@users.noreply.github.com>
2021-03-01 23:22:03 -0600

    Homepage 80% done.

--- dotgrowen/pages/index.js
+++ dotgrowen/pages/index.js
@@ -15,26 +15,50 @@ export default function Home() {
 
         <div className={styles.content}>
           <img className={styles.icon} src={"/icon-1.png"} />
-          <h3>. Project #0001</h3>
-          <small>WE ARE NOT THE SAME</small>
+          <h3 className={styles.content_title}>. Project #0001</h3>
+          <small className={styles.content_subtitle}>WE ARE NOT THE SAME</small>
         </div>
 
         <div className={styles.content}>
           <img className={styles.icon} src={"/icon-2.png"} />
-          <h3>. Project #0001</h3>
-          <small>WE ARE NOT THE SAME</small>
+          <h3 className={styles.content_title}>. Project #0002</h3>
+          <small className={styles.content_subtitle}>WE ARE NOT THE SAME</small>
         </div>
 
         <div className={styles.content}>
-          <img className={styles.icon} src={"/icon-3.png"} />
-          <h3>. Project #0001</h3>
-          <small>WE ARE NOT THE SAME</small>
+          <img className={styles.icon2} src={"/icon-3.png"} />
+          <h3 className={styles.content_title}>. Project #0003</h3>
+          <small className={styles.content_subtitle}>WE ARE NOT THE SAME</small>
         </div>
 
         <div className={styles.content}>
-          <img className={styles.icon} src={"/icon-4.png"} />
-          <h3>. Project #0001</h3>
-          <small>WE ARE NOT THE SAME</small>
+          <img className={styles.icon2} src={"/icon-4.png"} />
+          <h3 className={styles.content_title}>. Project #0004</h3>
+          <small className={styles.content_subtitle}>WE ARE NOT THE SAME</small>
+        </div>
+
+      </div>
+
+      {/**********************  BOTTOM SECTION  **********************/}
+
+      <div className={styles.bottom_area}>
+
+
+        <div className={styles.bottom_content}>
+          <div className={styles.bottom_box}>
+            <div className={styles.bottom_innerbox}></div>
+          </div>
+
+          <p> <span className={styles.green_dot}>.</span> Writing</p>
+        </div>
+
+
+        <div className={styles.bottom_content}>
+          <div className={styles.bottom_box}>
+            <div className={styles.bottom_innerbox}></div>
+          </div>
+
+          <p> <span className={styles.green_dot}>.</span> Projects</p>
         </div>
 
       </div>
